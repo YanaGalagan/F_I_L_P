9445703391bd5b04fa4572a2aa7eb9ccc2c41e68
Яна Александровна Галаган <s0160132@edu.kubsu.ru>
2022-03-09 21:54:55 +0300

    Добавить строку hello

--- ConsoleApp25/Program.fs
+++ ConsoleApp25/Program.fs
@@ -1,8 +1,10 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿
 
 open System
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
-    0 // return an integer exit code
+    printfn "Hello world"
+    
+    0 
