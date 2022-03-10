beb74658335ad54b1889a1bb8ec2b5360825dfc0
Яна Александровна Галаган <s0160132@edu.kubsu.ru>
2022-03-10 22:07:14 +0300

    Рекурсия вверх

---
+++ 5.4/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+
+let rec cifrSum n = 
+    if n = 0 then 0
+    else (n%10) + (cifrSum (n / 10))
+[<EntryPoint>]
+let main argv = 
+    System.Convert.ToInt32(System.Console.ReadLine()) |> cifrSum |> System.Console.WriteLine
+    let z = System.Console.ReadKey()
+    0 // возвращение целочисленного кода выхода
