774b56cba31c6a9f3b24e8e62259894b485d6d47
Яна Александровна Галаган <s0160132@edu.kubsu.ru>
2022-03-15 20:35:00 +0300

    5.14

---
+++ 5.14/Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+let obxod n func init=
+    let rec obxod1 n func init b=
+        if b=0 then init else
+        let newInit= if n%b=0 then func init b else init
+        let newb=b-1
+        obxod1 n func newInit newb 
+    obxod1 n func init n
+[<EntryPoint>]
+let main argv =
+    let a=System.Convert.ToInt32(System.Console.ReadLine())
+    System.Console.WriteLine("Сумма делителей числа:{0}", obxod a (fun x y -> x+y ) 0)
+    0 // return an integer exit code
