56c3761d213054c114da2f8d8df1b6e9e6c25dbc
Яна Александровна Галаган <s0160132@edu.kubsu.ru>
2022-03-15 21:21:11 +0300

    5.15

---
+++ 5.15/Program.fs
@@ -0,0 +1,23 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let rec nod n m=
+    if n=0||m=0 then n+m 
+    else
+    let newn=if n>m then n%m else n
+    let newm=if n<=m then m%n else m
+    nod newn newm
+
+let obrabotka n func init=
+    let rec obr n func init cand=
+        if cand<=0 then init else
+        let newInit= if nod n cand=1 then func init cand else init
+        let newcand=cand-1
+        obr n func newInit newcand
+    obr n func init n
+
+[<EntryPoint>]
+let main argv =
+    System.Console.WriteLine (obrabotka (System.Convert.ToInt32(Console.ReadLine())) (fun x y -> x+y) 0)
+    0 // return an integer exit code
