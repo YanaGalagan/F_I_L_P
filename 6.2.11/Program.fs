183d780a72de53989d781e941804c1b6139f596c
Яна Александровна Галаган <s0160132@edu.kubsu.ru>
2022-03-19 15:03:53 +0300

    6.12.11

---
+++ 6.2.11/Program.fs
@@ -0,0 +1,36 @@
+﻿// Learn more about F# at http://fsharp.org
+//дан в целочисленный массив, в котором лишь один элемент отличается. найти значение этого элемента
+//5 11 5 5 5 5
+open System
+
+let poisk list=
+    let rec p list  =
+        match list with
+        |[]-> list.Head
+        |h::t->
+        if list.Head<>list.Tail.Head then list.Tail.Head else p list.Tail
+    p list 
+let rec readList n = 
+     if n=0 then []
+     else
+     let Head = System.Convert.ToInt32(System.Console.ReadLine())
+     let Tail = readList (n-1)
+     Head::Tail
+ 
+let rec writeList = function
+     [] ->   let z = System.Console.ReadKey()
+             0
+     | (head : int)::tail -> 
+                        System.Console.WriteLine(head)
+                        writeList tail 
+                      
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "Введите количество элементов списка:"
+    let n = System.Convert.ToInt32(System.Console.ReadLine())
+    printfn "Введите элементы списка:"
+    let l= readList n
+    System.Console.WriteLine(poisk l)
+    0 // return an integer exit code
