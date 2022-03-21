3d802641487cce7646c925b55c142f3f38562caf
Яна Александровна Галаган <s0160132@edu.kubsu.ru>
2022-03-21 12:24:51 +0300

    6.12.27

---
+++ 6.12.27/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+//Дан целочисленный массив. Необходимо осуществить
+//циклический сдвиг элементов массива влево на одну позицию.
+open System
+let  izmenenie list=
+     match list with
+     |[]->list
+     |h::t -> t@[h]
+[<EntryPoint>]
+
+let main argv =
+    printfn "Введите количество элементов списка:"
+    let n = System.Convert.ToInt32(System.Console.ReadLine())
+    printfn "Введите элементы списка:"
+    Program.readList n|>izmenenie|>Program.writeList|>ignore
+    0 // return an integer exit code
