25dbddfc7278d5ad957f06a00a79db2f43b9e783
Яна Александровна Галаган <s0160132@edu.kubsu.ru>
2022-03-29 22:08:44 +0300

    7.23

---
+++ 7.23/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+(*1.23
+Дан целочисленный массив. Необходимо найти два наименьших
+элемента.*)
+open System
+
+[<EntryPoint>]
+let main argv =
+    let l=Program.readData
+    let min=List.min l
+    Console.WriteLine(" ")
+    Console.WriteLine(min)
+    Console.WriteLine(" ")
+    let newl=List.filter (fun x -> x<>min) l
+    Console.WriteLine(List.min newl)
+    0 // return an integer exit code
